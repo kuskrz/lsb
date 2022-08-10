cdc43402106b0ddf0a8787384db17a1ccf5502ff
kkus <kkus@fedora>
2022-08-10 23:35:14 +0200

    wcommit - files removal

--- lsb/src/main/java/kus/krzysztof/domain/Children.java
+++
@@ -1,5 +0,0 @@
-package kus.krzysztof.domain;
-
-public class Children {
-
-}
--- lsb/src/main/java/kus/krzysztof/domain/Mother.java
+++
@@ -1,8 +0,0 @@
-package kus.krzysztof.domain;
-
-import javax.persistence.Entity;
-
-@Entity
-public class Mother {
-
-}
